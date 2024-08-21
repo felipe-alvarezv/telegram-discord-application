27ac00cd33533a76b6e1b9c0840b764396c7340b
Felipe Alvarez <felipealvarezv117@hotmail.com>
2024-08-21 15:11:14 -0400

    Addition of all functionality, fixing GUI

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
 import tkinter as tk
 from tkinter_functions import main_page_click
-from tkinter import ttk
-
 from telegram_functions import initialize, listen
 
 def main():
-    # Initialize client with config data
-    telegram_client = initialize()
-
+    #Initialize Tkinter window, set default size, and application name
     root = tk.Tk()
-    root.minsize(250, 125)
-    root.maxsize(400, 400)
     root.title("Telegram to Discord App")
+    root.resizable(width=False, height=False)
 
-    notebook = ttk.Notebook(root)
-    notebook.pack(expand=True, fill='both')
-
-
-    infor_frame = tk.Frame(root)
-    manage_frame = tk.Frame(root)
-
-    frame = tk.Frame(root)
-    frame.grid(row=0, column=0)
-
-    main_page_click(root, frame, telegram_client)
+    main_page_click(root)
     root.mainloop()
 
 if __name__ == '__main__':
--- tkinter_functions.py
+++ tkinter_functions.py
@@ -1,90 +1,147 @@
 import os
 import pickle
 import tkinter as tk
-from Channel import Channel
+from tkinter import Toplevel
+from tkinter import ttk
+from tkinter import Scrollbar
+from tkinter import messagebox
+from Channel import Channel 
+from load_functions import load_channel_list
 from telegram_functions import initialize, listen
 
 #Main Page
-def main_page_click(root, frame, telegram_client):    
-    frame.destroy()
-    frame = tk.Frame(root)
-    frame.grid(row=0, column=0)
+def main_page_click(root):
+    # Initialize Telegram client with config data
+    telegram_client = initialize()
 
-    #Align buttons in the center
-    frame.columnconfigure(0, weight=1)
+    #Initialize Tkinter notebook to store tabs
+    notebook = ttk.Notebook(root)
+    notebook.pack(expand=True, fill='both')
 
-    info_btn = tk.Button(frame, text="Information", width=20, command=lambda: info_btn_click(root, frame, telegram_client))
-    info_btn.grid(row=0, column=0)
+    #Information Tab
+    info_frame = tk.Frame(root)
+    info_frame.pack()
 
-    channel_btn = tk.Button(frame, text="Channel Management", width=20, command=lambda: channel_btn_click(root, frame, telegram_client))
-    channel_btn.grid(row=1, column=0)
-
-    application_btn = tk.Button(frame, text="Start Application", width=20, command=lambda: application_btn_click(root, frame, telegram_client))
-    application_btn.grid(row=2, column=0)
-
-#Information Page
-def info_btn_click(root, frame, telegram_client): 
-    frame.destroy()
-    frame = tk.Frame(root)
-    frame.grid(row=0, column=0)
-
-    info_label = tk.Label(frame, text="The purpose of this application is to foward messages from a Telegram channel to a Discord webhook.\nMedia is not currently supported.")
+    #Information Tab - Widgets
+    info_label = tk.Label(info_frame, text="The purpose of this application is to foward text messages from a Telegram channel to a Discord channel webhook.")
     info_label.grid(row=0, column=0)
     
-    author_label = tk.Label(frame, text="Made by Felipe Alvarez")
-    author_label.grid(row=1, column=0)
+    media_label = tk.Label(info_frame, text='Media is not currently supported!')
+    media_label.grid(row=1, column=0, pady=10)
 
-    return_button = tk.Button(frame, text="Main Page", command=lambda: main_page_click(root, frame, telegram_client))
-    return_button.grid(row=2, column=0)
+    info_start_btn = tk.Button(info_frame, text="Start Application", command=lambda: start_application(root, telegram_client))
+    info_start_btn.grid(row=2, column=0, pady=40)
 
-#Channel Management Page
-def channel_btn_click(root, frame, telegram_client):
-    frame.destroy()
-    frame = tk.Frame(root)
-    frame.grid(row=0, column=0)
+    author_label = tk.Label(info_frame, text="Made by Felipe Alvarez")
+    author_label.grid(row=3, column=0)
 
-    directory = './channels/'
-    channels = []
-    
-    for file in os.listdir(directory):
-        if file.endswith('.pkl'):
-            with open(directory + file, 'rb') as f:
-                temp_channel = pickle.load(f)
-                channels.append([temp_channel.get_desc(), temp_channel.get_id(), temp_channel.get_webhook()])
-    
-    #Create listbox and add all existing channel files
-    channel_list = tk.Listbox(frame, width=50)
-    channel_list.grid(row=0, column=0)
+    #Channel Management Tab
+    manage_frame = tk.Frame(root)
+    manage_frame.pack()
 
+    #Channel Management Tab - Frames
+    manage_left_frame_top = tk.Frame(manage_frame)
+    manage_left_frame_top.grid(row=0, column=0)
+
+    manage_left_frame_bottom = tk.Frame(manage_frame)
+    manage_left_frame_bottom.grid(row=1, column=0)
+
+    manage_right_frame_top = tk.Frame(manage_frame)
+    manage_right_frame_top.grid(row=0, column=1)
+
+    manage_right_frame_bottom = tk.Frame(manage_frame)
+    manage_right_frame_bottom.grid(row=1, column=1)
+
+    channels = load_channel_list() #Load channel objects as a list
+
+    #Create Treeview for channels
+    channel_tree = ttk.Treeview(manage_left_frame_top, column=('c1', 'c2', 'c3'), show='headings', height=8, selectmode='browse')
+    channel_tree.column('#1', width=200, anchor='center')
+    channel_tree.heading('#1', text='Description')
+    channel_tree.column('#2', width=0, stretch = "no")
+    channel_tree.heading('#2', text='ID')
+    channel_tree.column("#3", width = 0, stretch = "no")
+    channel_tree.heading('#3', text='Webhook')
+    channel_tree.grid(row=0, column=1, padx=5, pady=5)
+    channel_tree.bind('<ButtonRelease-1>', lambda event: show_channel_info(event, channel_tree, channel_info_list))
+    channel_tree.bind('<KeyRelease-Up>', lambda event: show_channel_info(event, channel_tree, channel_info_list))
+    channel_tree.bind('<KeyRelease-Down>', lambda event: show_channel_info(event, channel_tree, channel_info_list))
+
+    channel_tree_scroll = Scrollbar(manage_left_frame_top, orient=tk.VERTICAL, command=channel_tree.yview)
+    channel_tree['yscrollcommand'] = channel_tree_scroll.set
+    channel_tree_scroll.grid(row=0, column=0, sticky='ns')
+
+    #Add all loaded channels to Listbox
     for channel in channels:
-        channel_list.insert("end", channel)
+        channel_tree.insert('', 'end', text='1', values=channel)
 
-    channel_add = tk.Button(frame, text="+", command=lambda: channel_add_click(root, frame, telegram_client))
-    channel_add.grid(row=0, column=1)
+    #Add Channel Button Widget
+    add_btn = tk.Button(manage_right_frame_bottom, text="Add Channel", command=lambda: add_channel_btn(root, telegram_client, channel_tree))
+    add_btn.grid(row=0, column=0)
 
-    channel_modify = tk.Button(frame, text="M", command=lambda: channel_modify_click(root, frame, channel_list, telegram_client))
-    channel_modify.grid(row=0, column=2)
+    #Channel Information Listbox Widget
+    channel_info_list = tk.Listbox(manage_right_frame_top, width=80, height=6)
+    channel_info_list.grid(row=0, column=0, padx=5, pady=5, sticky='n')
 
-    channel_delete = tk.Button(frame, text="-", command=lambda: channel_delete_click(root, frame, channel_list, telegram_client))
-    channel_delete.grid(row=0, column=3)
+    info_scroll = Scrollbar(manage_right_frame_top, orient=tk.HORIZONTAL, command=channel_info_list.xview)
+    channel_info_list['xscrollcommand'] = info_scroll.set
+    info_scroll.grid(row=1, column=0, sticky='ew')
 
-    return_button = tk.Button(frame, text="Main Page", command=lambda: main_page_click(root, frame, telegram_client))
-    return_button.grid(row=1, column=0)
+    #Delete Channel Button Widget
+    delete_btn = tk.Button(manage_right_frame_bottom, text='Delete Channel', command=lambda: delete_channel_btn(root, telegram_client, channel_tree))
+    delete_btn.grid(row=0, column=1)
 
-#Application Page
-def application_btn_click(root, frame, telegram_client):
-    frame.destroy()
-    frame = tk.Frame(root)
-    frame.grid(row=0, column=0)
+    #Set the names of the tabs
+    notebook.add(info_frame, text="Information")
+    notebook.add(manage_frame, text="Channel Management")
 
-    root.destroy()
-    listen(telegram_client)
+def show_channel_info(event, channel_tree, channel_info_list):
+    selection = channel_tree.selection() #Get the selected item of the tree
+    channel_info_list.delete(0, 'end') #Delete the previous info displayed
 
-#Add Channel Page
-def channel_add_click(root, frame, telegram_client):
-    frame.destroy()
-    frame = tk.Frame(root)
-    frame.grid(row=0, column=0)
+    if selection:
+        #Get the selected channel
+        data = channel_tree.item(selection)['values']
+
+        #Add the channel's information to the list
+        for item in data:
+            channel_info_list.insert("end", item)
+
+def add_channel_btn(root, telegram_client, channel_tree):
+    #Create a new window with for add channel widgets
+    add_channel_window = Toplevel(root)
+    add_channel_window.title("Add Channel")
+    add_channel_window.grab_set()
+    add_channel_window.resizable(width=False, height=False)
+
+    #Geometry to place new window on top of root
+    x = root.winfo_x()
+    y = root.winfo_y()
+    add_channel_window.geometry("+%d+%d" % (x + 80, y + 60))
+
+    #Frames for right and left side
+    left_side = tk.Frame(add_channel_window)
+    left_side.grid(row=0, column=0)
+
+    right_side = tk.Frame(add_channel_window)
+    right_side.grid(row=0, column=1)
+
+    bottom_left = tk.Frame(add_channel_window)
+    bottom_left.grid(row=1, column=0)
+
+    bottom_right = tk.Frame(add_channel_window)
+    bottom_right.grid(row=1, column=1)
+
+    #Widgets for Description
+    description_label = tk.Label(left_side, text='Description:')
+    description_label.grid(row=0, column=0, sticky='w', pady=(0, 5))
+
+    description_entry = tk.Entry(right_side, width=44)
+    description_entry.grid(row=0, column=0, pady=(0, 5))
+
+    #Widgets for ID
+    id_label = tk.Label(left_side, text="ID:")
+    id_label.grid(row=1, column=0, sticky='w', pady=(0, 5))
 
     #Varianble to store existing channels
     channels = []
@@ -93,130 +150,66 @@ def channel_add_click(root, frame, telegram_client):
             temp_channel = [dialog.title, dialog.id * -1]
             channels.append(temp_channel)
 
-    #Variable to store selected channel
-    selected_channel = tk.Variable(frame)
+    selected_channel = tk.Variable(right_side)
 
-    description_label = tk.Label(frame, text="Description:")
-    description_label.grid(row=0, column=0)
+    id_options = tk.OptionMenu(right_side, selected_channel, *channels)
+    id_options.grid(row=1, column=0, sticky='ew', pady=(0, 5))
 
-    description_entry = tk.Entry(frame)
-    description_entry.grid(row=0, column=1)
+    #Widgets for Webhook
+    webhook_label = tk.Label(left_side, text="Webhook:")
+    webhook_label.grid(row=2, column=0, sticky='w', pady=(0, 5))
 
-    channel_label = tk.Label(frame, text="Channel:")
-    channel_label.grid(row=1, column=0)
+    webhook_entry = tk.Entry(right_side)
+    webhook_entry.grid(row=2, column=0, sticky='ew', pady=(0, 5))
 
-    channel_id = tk.OptionMenu(frame, selected_channel, *channels)
-    channel_id.grid(row=1, column=1)
+    #Widget for submition
+    submit_button = tk.Button(bottom_right, text="Submit", command=lambda: add_channel_btn_clicked(add_channel_window, channel_tree, description_entry, selected_channel, webhook_entry))
+    submit_button.grid(row=0, column=0, pady=5)
 
-    discord_label = tk.Label(frame, text="Discord Webhook:")
-    discord_label.grid(row=2, column=0)
+    add_channel_window.grab_set()
 
-    discord_webhook = tk.Entry(frame)
-    discord_webhook.grid(row=2, column=1)
-
-    add_channel_btn = tk.Button(frame, text="Add Channel", command=lambda: add_channel_btn_clicked(root, frame, description_entry, selected_channel, discord_webhook, telegram_client))
-    add_channel_btn.grid(row=3, column=0)
-
-    discard_channel_btn = tk.Button(frame, text="Discard", command=lambda: channel_btn_click(root, frame, telegram_client))
-    discard_channel_btn.grid(row=3, column=1)
 
 #Add Channel Button Event
-def add_channel_btn_clicked(root, frame, description_entry, selected_channel, discord_webhook, telegram_client):
-    if description_entry.get() and selected_channel and discord_webhook.get():
+def add_channel_btn_clicked(add_channel_window, channel_tree, description_entry, selected_channel, webhook_entry):
+    if description_entry.get() and selected_channel and webhook_entry.get():
         filename = 'channels/' + description_entry.get() + '.pkl'
         channel_file = open(filename, 'wb')
-        temp_channel = Channel(description_entry.get(), selected_channel.get()[1], discord_webhook.get()) #Make a Channel object with the passed variable
+        temp_channel = Channel(description_entry.get(), selected_channel.get()[1], webhook_entry.get()) #Make a Channel object with the passed variable
         pickle.dump(temp_channel, channel_file)
         channel_file.close()
+        add_channel_window.destroy()
 
-        frame.destroy()
-        channel_btn_click(root, frame, telegram_client)
-    else:
-        frame.destroy()
-        channel_add_click(root, frame, telegram_client)
+        channel_tree.delete(*channel_tree.get_children())  #Clear Treeview
+        channels = load_channel_list() #Load channel objects as a list
 
-#Modify Channel Page
-def channel_modify_click(root, frame, channel_list, telegram_client):
-    #Variable to store channel object
-    channel = []
+        #Add all loaded Channels to Treeview
+        for channel in channels:
+            channel_tree.insert('', 'end', text='1', values=channel)
 
-    for i in channel_list.curselection():
-        channel = channel_list.get(i)
+        
 
-    frame.destroy()
-    frame = tk.Frame(root)
-    frame.grid(row=0, column=0)
+#Delete Channel Button Event
+def delete_channel_btn(root, telegram_client, channel_tree):
+    selection = channel_tree.selection() #Get the selected item of the tree
 
-    if channel:
-        description_label = tk.Label(frame, text="Description:")
-        description_label.grid(row=1, column=0)
+    if selection:
+        response = messagebox.askquestion('Delete Channel', 'Are you sure you want to delete the channel?')
 
-        description_entry = tk.Entry(frame)
-        description_entry.grid(row=1, column=1)
-        description_entry.insert("end", channel[0])
+        if response == 'yes':
+            #Get the selected channel description
+            data = channel_tree.item(selection)['values'][0]
 
-        discord_label = tk.Label(frame, text="Discord Webhook:")
-        discord_label.grid(row=2, column=0)
+            if data:
+                filename = 'channels/' + data + '.pkl'
+                os.remove(filename) #Delete channel file
 
-        discord_webhook = tk.Entry(frame)
-        discord_webhook.grid(row=2, column=1)
-        discord_webhook.insert("end", channel[2])
+            channel_tree.delete(*channel_tree.get_children())  #Clear Treeview
+            channels = load_channel_list() #Load channel objects as a list
 
-        save_channel_btn = tk.Button(frame, text="Save", command=lambda: save_channel_btn_clicked(root, frame, channel, description_entry, discord_webhook, telegram_client))
-        save_channel_btn.grid(row=3, column =0)
+            #Add all loaded Channels to Treeview
+            for channel in channels:
+                channel_tree.insert('', 'end', text='1', values=channel)
 
-        discard_channel_btn = tk.Button(frame, text="Discard", command=lambda: channel_btn_click(root, frame, telegram_client))
-        discard_channel_btn.grid(row=3, column=1)
-
-#Save Modified Channel Event
-def save_channel_btn_clicked(root, frame, channel, description_entry, discord_webhook, telegram_client):
-    if description_entry and discord_webhook:
-        channel_description = description_entry.get()
-
-        original_filename = 'channels/' + channel[0] + '.pkl'
-        filename = 'channels/' + channel_description + '.pkl'
-        channel_file = open(filename, 'wb')
-
-        os.remove(original_filename) #Delete original channel files
-        temp_channel = Channel(channel_description, channel[1], discord_webhook.get()) #Make a Channel object with the passed variable
-        pickle.dump(temp_channel, channel_file)
-        channel_file.close()
-
-        frame.destroy()
-        channel_btn_click(root, frame, telegram_client)
-    else:
-        frame.destroy()
-        channel_add_click(root, frame, telegram_client)
-
-    frame.destroy()
-    channel_btn_click(root, frame, telegram_client)
-
-#Delete Channel Page
-def channel_delete_click(root, frame, channel_list, telegram_client):
-    #Variable to store channel object
-    channel_id = []
-
-    for i in channel_list.curselection():
-        channel = channel_list.get(i)
-
-    frame.destroy()
-    frame = tk.Frame(root)
-    frame.grid(row=0, column=0)
-
-    delete_label = tk.Label(frame, text="Are you sure you want to delete this channel?")
-    delete_label.grid(row=0, column=0)
-
-    delete_channel_btn = tk.Button(frame, text="Yes", command=lambda: delete_channel_btn_clicked(root, frame, channel, telegram_client)) 
-    delete_channel_btn.grid(row=0, column=1)
-
-    cancel_delete_btn = tk.Button(frame, text="No", command=lambda: channel_btn_click(root, frame, telegram_client))
-    cancel_delete_btn.grid(row=0, column=2)
-
-#Delete Channel Event
-def delete_channel_btn_clicked(root, frame, channel, telegram_client):
-     if channel:
-        filename = 'channels/' + channel[0] + '.pkl'
-        os.remove(filename) #Delete channel file
-
-        frame.destroy()
-        channel_btn_click(root, frame, telegram_client)
+def start_application(root, telegram_client):
+    root.destroy()
+    listen(telegram_client)
